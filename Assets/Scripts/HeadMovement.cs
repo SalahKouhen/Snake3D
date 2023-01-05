c8b62291d94ca81041f92bca20fed945ea535afd
Salah Kouhen <kouhen@OXAOPPWTLAP33.physics.ox.ac.uk>
2023-01-05 00:28:20 +0000

    phone movement system

--- Assets/Scripts/HeadMovement.cs
+++ Assets/Scripts/HeadMovement.cs
@@ -4,17 +4,63 @@ using UnityEngine;
 
 public class HeadMovement : MonoBehaviour
 {
-    // Get the mouse position
 
-    // Start is called before the first frame update
+    [Header("mouse controls")]
+    private Vector3 mouseFirstPos;
+    private Vector3 mouseOffset;
+    private bool firstClickFlag;
+
+    [Header("pitching, yaw and roll")]
+    public Vector3 turnTorque = new Vector3(90f, 25f, 45f);
+    private Rigidbody rb;
+    public float yaw;
+    public float pitch;
+    public float forceMult = 1f;
+    private float dragFactor = 10f;
+
+
     void Start()
     {
-        
-    }
+        firstClickFlag = true;
+        rb = GetComponent<Rigidbody>();
 
-    // Update is called once per frame
+    }
     void Update()
     {
-        
+        //Debug.Log(firstClickFlag);
+        if (Input.GetMouseButton(0))
+        {
+            if (firstClickFlag)
+            {
+                mouseFirstPos = Input.mousePosition;
+                firstClickFlag = false;
+            }
+            mouseOffset = Input.mousePosition - mouseFirstPos;
+            Debug.Log(mouseOffset.x);
+
+        }
+        else
+        {
+            firstClickFlag = true;
+            mouseOffset.x = 0f;
+            mouseOffset.y = 0f;
+        }
+
+        yaw = mouseOffset.x;
+        pitch = mouseOffset.y;
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddRelativeTorque(new Vector3(-turnTorque.x * pitch,
+                                                turnTorque.y * yaw,
+                                                -turnTorque.z * 0f) * forceMult,
+                                    ForceMode.Force);
+        //damp the rotation
+        rb.AddTorque(-rb.angularVelocity * dragFactor);
+    }
+    private Quaternion Damp(Quaternion a, Quaternion b, float lambda, float dt)
+    {
+        return Quaternion.Slerp(a, b, 1 - Mathf.Exp(-lambda * dt));
     }
 }
