871f54bf4a681d9a5bb3a8e18d463ffde3c788fc
Salah Kouhen <kouhen@OXAOPPWTLAP33.physics.ox.ac.uk>
2023-01-05 04:35:51 +0000

    added eating and spawner for food

---
+++ Assets/Scripts/FoodSpawn.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodSpawn : MonoBehaviour
+{
+    public GameObject food;
+    public int foodConst = 100;
+    public int foodNum = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < foodConst; i++)
+        {
+            Instantiate(food, randomSpawn(), food.transform.rotation);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (foodNum < foodConst)
+        {
+            Instantiate(food, randomSpawn(), food.transform.rotation);
+            foodNum += 1;
+        }
+    }
+
+    Vector3 randomSpawn()
+    {
+        return new Vector3(Mathf.Ceil(Random.Range(-49, 43)), Mathf.Ceil(Random.Range(3, 87)), Mathf.Ceil(Random.Range(-49, 43)));
+    }
+}
