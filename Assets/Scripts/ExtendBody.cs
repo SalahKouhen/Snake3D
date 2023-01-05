57c6fd581c59241d1efb2e845821824caaf2c3cb
colakovics98 <colakovics98@gmail.com>
2023-01-05 03:02:03 +0000

    Start of work

--- Assets/Scripts/ExtendBody.cs
+++ Assets/Scripts/ExtendBody.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 
 public class ExtendBody : MonoBehaviour
 {
-    
+    public Transform lastSegment;
+    public Transform tail;
+    public GameObject bodySegmentPrefab;
+
+    void ExtendSnake()
+    {
+        Vector3 backwards = lastSegment.up * -1;
+    }
 }
